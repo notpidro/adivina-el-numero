9828ead633d8549b9ea8a02809cebbc8ae6f1eb3
Walter F. Campillay P <walterfabian.cp@hotmail.com>
2023-05-24 18:14:19 -0300

    agrego primera funcion controlarNumero

--- adivina-numero.js
+++ adivina-numero.js
@@ -15,24 +15,45 @@ slider.addEventListener("click", () => {
 setTheme(localStorage.getItem("theme") || preferedColorScheme);
 //
 
-
-const fallidos = document.querySelector(".fallidos")
-const ultimoResultado = document.querySelector(".ultimo-resultado")
-const mayorMenor = document.querySelector(".mayot-menor")
-const enviarAdivina = document.querySelector(".enviar-adivina")
-const inputAdivina = document.querySelector(".input-adivina")
+const fallidos = document.querySelector(".fallidos");
+const ultimoResultado = document.querySelector(".ultimo-resultado");
+const mayorMenor = document.querySelector(".mayot-menor");
+const enviarAdivina = document.querySelector(".enviar-adivina");
+const inputAdivina = document.querySelector(".input-adivina");
 
 let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-let cantidadIntentos = 1
+let cantidadIntentos = 1;
 let btnReseteo;
 
 function controlarNumero() {
-    const numeroIngresado = Number(inputAdivina.value)
-    if (cantidadIntentos === 1) {
-        fallidos = "Numero ingresados antes: "
-    }
+	const numeroIngresado = Number(inputAdivina.value);
+	if (cantidadIntentos === 1) {
+		fallidos = "Numero ingresados antes: ";
+	}
 
-    fallidos.textContent += numeroIngresado + " "
+	fallidos.textContent += numeroIngresado + " ";
 
-    
+	if (numeroIngresado === numeroAleatorio) {
+		ultimoResultado.textContent = "Felicitaciones ! Adivinaste el numero !";
+		ultimoResultado.style.backgroundColor = "green";
+		mayorMenor.textContent = "";
+		finDelJuego();
+	} else if (cantidadIntentos === 10) {
+		ultimoResultado = "Perdiste ! Fin del juego !";
+		mayorMenor.textContent = "";
+		finDelJuego();
+	} else {
+		ultimoResultado.textContent = "Fallaste !";
+		ultimoResultado.style.backgroundColor = "red";
+		if (numeroIngresado < numeroAleatorio) {
+			mayorMenor.textContent = "El numero que ingresaste es bajo";
+		} else if (numeroIngresado > numeroAleatorio) {
+			mayorMenor.textContent = "El numero que ingresaste es mas bajo";
+		}
+	}
+	cantidadIntentos++;
+	inputAdivina.value = "";
+	inputAdivina.focus();
 }
+
+enviarAdivina.addEventListener("click", controlarNumero);
